1958a6aa511ff4e5941d2268e3f0a40c45f5be8b
EsmiK64 <94920591+EsmiK64@users.noreply.github.com>
2023-04-16 20:09:02 +0200

    Created THE BLOB

---
+++ src/components/components.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Heading = styled.h1`
+  color: red;
+`;
+
+const PathContainer = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  height: 70vh;
+  width: 70vw;
+`;
+
+export default {
+  Heading,
+  PathContainer,
+};
