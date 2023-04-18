d4c4720606186cd3c10ab6d971c19dea086ad3ce
EsmiK <s.simsa@centrum.cz>
2023-04-18 08:00:14 +0200

    Added body element

--- src/components/components.tsx
+++ src/components/components.tsx
@@ -1,16 +1,21 @@
 import styled from "styled-components";
 
-const Heading = styled.h1`
+export const Heading = styled.h1`
   color: red;
 `;
 
-const PathContainer = styled.div`
+export const PathContainer = styled.div`
   position: absolute;
   left: 0;
   bottom: 0;
   height: 70vh;
   width: 70vw;
 `;
+
+export const Body = styled.body`
+  background: transparent;
+  background-color: #eee;
+`
 
 export default {
   Heading,
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { Body } from '../components/components'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>EsmiK's portfolio</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
